d55610986364f3ee00a9edf255d12e10268dcc4c
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2021-06-26 01:10:01 -0500

    General code fixed. Weights matrix generations fixed. MatMult in forward fixed. Activation functions implemented.

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ from utils import *
             output = self.sigmoid(output)
             return output'''
 
-def getRandomMatrix(*dimensions): # Pero para que necesitas esto si dimensions en primer lugar ya es una lista XD
-    return np.random.random_sample(dimensions) #todo bien manito? XD
+def getRandomMatrix(*dimensions):
+    return np.random.random_sample(dimensions)
 
 def RELU(matrix):
+    print("executing RELU")
     new_matrix = []
     for i in np.nditer(matrix):
         if i < 0:
@@ -30,45 +31,70 @@ def RELU(matrix):
     new_matrix = np.array(new_matrix)
     return new_matrix.reshape(matrix.shape)
 
+def sigmoid(matrix):
+    print("executing sigmoid")
+    new_matrix = []
+    for i in matrix:
+        new_matrix.append(1.0/(1+np.exp(-i)))
+    return np.array(new_matrix)
+
+def tanh(matrix):
+    print("executing tanh")
+    new_matrix = []
+    for i in matrix:
+        new_matrix.append((np.exp(i)-np.exp(-i))/(np.exp(i)+np.exp(-i)))
+    return np.array(new_matrix)
+
+
 def SoftMax():
     return 
 
 
-# Sigmoid, RELU, 
 class MLP:
     def __init__(self, hidden_num, layer_functs, node_num):
         # (self, int, [funciones], [ints])
         print("me construyo")
         assert(hidden_num == len(layer_functs))
-        assert(hidden_num == len(node_num))
+        assert(hidden_num == len(node_num)-1) #notar que no deberiamos contar la capa de input porque no hay una funcion pasando por ahi
         self.input_size = node_num[0]
         self.layers = layer_functs
-        self.w = [getRandomMatrix(1,i) for i in node_num]
+        self.w = []
+        for i in range(1,len(node_num)):
+            self.w.append(getRandomMatrix(node_num[i],node_num[i-1]))
         for ws in self.w:
-            print(ws)         
+            print("-",ws)         
 
     
     #w {#neuronas capa 1, X} * input {X, 1}   = ans {#neuronas, 1} => layers[i](ans)
 
     def forward(self, item):
-        temp_ans = item
+        temp_ans = np.array([item])
         for i in range(len(self.layers)):
-            temp_ans = self.w[i] * temp_ans.T
+            print("Layer", i+1)
+            print("arr shape    :", temp_ans.shape)
+            print("w   shape    :", self.w[i].T.shape)
+            temp_ans = np.dot(temp_ans,self.w[i].T)
+            print("arr after    :", temp_ans.shape)
             temp_ans = self.layers[i](temp_ans)
+            print("arr activated:", temp_ans.shape)
+            print("----------")
         return temp_ans
     
     def backPropagate(self,output,expected_output):
-        xd = 0 
+        xd = 0
         
     def execute(self, dataset):
+        counter = 1
         for item in dataset:
+            print("COUNTER #",counter)
             output = self.forward(np.array(item[0]).astype("float"))
             self.backPropagate(output,item[1])
+            return
             
             
 
 if __name__ == '__main__':
-    mlp = MLP(3,[RELU,'sigmoid','tanh'],[30,20,10])
+    mlp = MLP(4,[RELU,sigmoid,RELU,tanh],[30,20,40,20,10])
     dataset = read_db()
     mlp.execute(dataset)    
     # def forward():
